bd6f8c92ee9f14b6d1b8677837beb345ce2054e8
Bartosz Kościug <bartosz@cloudify.co>
2022-06-08 23:14:48 +0200

    Adding license from secret key.

--- common.groovy
+++ common.groovy
@@ -39,7 +39,7 @@ def testBlueprints(){
     ssh -i ~/.ssh/ec2_ssh_key -l centos \$(cat capabilities.json | jq '.endpoint.value' | tr -d '"') <<'EOT'
 sudo pip3 install -U pytest
 cd /tmp
-python3 -m pytest --tb=long -v -ra test_blueprints.py
+python3 -m pytest --tb=long -v -ra -o log_cli=True test_blueprints.py
 EOT
 """
 }
