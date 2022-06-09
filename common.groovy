ecdb5dac56a24a517d55eadade61dc25f6d1f822
Bartosz Kościug <bartosz@cloudify.co>
2022-06-09 12:55:49 +0200

    Change test framework.

--- common.groovy
+++ common.groovy
@@ -37,7 +37,7 @@ def testBlueprints(){
     scp -i ~/.ssh/ec2_ssh_key test-blueprints.json centos@\$(cat capabilities.json | jq '.endpoint.value' | tr -d '"'):/home/centos
     scp -i ~/.ssh/ec2_ssh_key parse_tests.py centos@\$(cat capabilities.json | jq '.endpoint.value' | tr -d '"'):/home/centos
     ssh -i ~/.ssh/ec2_ssh_key -l centos \$(cat capabilities.json | jq '.endpoint.value' | tr -d '"') <<'EOT'
-sudo pip3 install -U parametrized
+sudo pip3 install -U parameterized
 cd /home/centos
 python3 test_blueprints.py
 EOT
