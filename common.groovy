c160aa48c977f3be467850c90b41dd00c417fed0
Bartosz Kościug <bartosz@cloudify.co>
2022-06-09 12:30:25 +0200

    Change test framework.

--- common.groovy
+++ common.groovy
@@ -37,9 +37,9 @@ def testBlueprints(){
     scp -i ~/.ssh/ec2_ssh_key test-blueprints.json centos@\$(cat capabilities.json | jq '.endpoint.value' | tr -d '"'):/home/centos
     scp -i ~/.ssh/ec2_ssh_key parse_tests.py centos@\$(cat capabilities.json | jq '.endpoint.value' | tr -d '"'):/home/centos
     ssh -i ~/.ssh/ec2_ssh_key -l centos \$(cat capabilities.json | jq '.endpoint.value' | tr -d '"') <<'EOT'
-sudo pip3 install -U pytest
+sudo pip3 install -U parametrized
 cd /home/centos
-pytest
+python3 test_blueprints.py
 EOT
 """
 }
--- test_blueprints.py
+++ test_blueprints.py
@@ -1,15 +1,24 @@
+from parameterized import parameterized
 from parse_tests import ParseTestData
 import subprocess
-import pytest
+import unittest
 
-test_data = ParseTestData() 
+test_data = ParseTestData()
 ids = test_data.get_ids()
 args = test_data.get_args()
 args_uninstall = test_data.get_args_uninstall()
 
-@pytest.mark.parametrize('blueprint', ids, ids=ids)
-def test_blueprints(blueprint):
-    proc_install = subprocess.run(args.get(blueprint))
-    proc_uninstall = subprocess.run(args_uninstall.get(blueprint), timeout=300)
-    assert proc_install.returncode == 0 and proc_uninstall.returncode == 0
-   
+
+class TestBlueprint(unittest.TestCase):
+
+    def test_blueprints(self):
+        for blueprint in ids:
+            with self.subTest(blueprint):
+                proc_install = subprocess.run(args.get(blueprint))
+                proc_uninstall = subprocess.run(
+                    args_uninstall.get(blueprint), timeout=300)
+                self.assertTrue(proc_install.returncode ==
+                            0 and proc_uninstall.returncode == 0)
+
+if __name__ == "__main__":
+    unittest.main()
