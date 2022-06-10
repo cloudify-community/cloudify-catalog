5295a5e5b00c137309b4e765d9579502342e747e
Bartosz Kościug <bartosz@cloudify.co>
2022-06-10 15:26:48 +0200

    upload bps.

--- test_blueprints.py
+++ test_blueprints.py
@@ -14,7 +14,7 @@ class TestBlueprints(unittest.TestCase):
     def upload(self):
         for blueprint in ids: 
             with self.subTest(blueprint):
-                proc_upload = subprocess.run(args_upload(blueprint))
+                proc_upload = subprocess.run(args_upload.get(blueprint))
                 self.assertTrue(proc_upload.returncode == 0)
     
     def install(self):
