d09b008d068180ba99da690371425da5a1e5d73d
Bartosz Kosciug <bartosz@cloudify.co>
2022-11-07 10:06:00 +0100

    PR reviews.

--- other/secrets_validation/scripts/secrets_validation.py
+++ other/secrets_validation/scripts/secrets_validation.py
@@ -8,18 +8,8 @@ from cloudify.state import ctx_parameters as inputs
 sys.tracebacklimit = -1
 
 client = get_rest_client()
-provider = inputs["provider"].lower()
+secrets = [secret.lower() for secret in inputs["secrets"]]
 missing = []
-
-if provider == "aws":
-    secrets = ["aws_access_key_id", "aws_secret_access_key"]
-elif provider == "azure":
-    secrets = ["azure_tenant_id", "azure_client_id",
-               "azure_subscription_id", "azure_client_secret"]
-elif provider == "gcp":
-    secrets = ["gcp_credentials"]
-else:
-    raise NonRecoverableError("The provider name should be: aws, azure or gcp")    
 
 for secret in secrets:
     try:
@@ -28,7 +18,7 @@ for secret in secrets:
         missing.append(secret)
 
 if missing:
-    ctx.logger.error("Please, create missing {} secret value for: {}".format(
-        provider.upper(), " and ".join(missing)))
-    raise NonRecoverableError("Missing {} provider secret value for: {}".format(
-        provider.upper(), " and ".join(missing)))
+    ctx.logger.error(
+        "Please, create missing secret value for: {}".format(" and ".join(missing)))
+    raise NonRecoverableError(
+        "Missing {} provider secret value for: {}".format(" and ".join(missing)))
