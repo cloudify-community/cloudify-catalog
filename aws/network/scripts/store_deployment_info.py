4aa4c3212cfffb226e95f844de2a613589f4772e
bartoszkosciug <97445164+bartoszkosciug@users.noreply.github.com>
2022-11-14 08:56:37 +0100

    Delete store_deployment_info.py

--- aws/network/scripts/store_deployment_info.py
+++
@@ -1,9 +0,0 @@
-import json
-from os import path
-
-from cloudify import ctx
-
-INFO_FILE_PATH = path.join("/", "tmp", "vpc-example-vars.json")
-
-with open(INFO_FILE_PATH, "r") as vars_file:
-    ctx.instance.runtime_properties.update(json.load(vars_file))
