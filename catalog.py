35a7a939e8051c8f898a4084eac02ada9ac8d721
Bartosz Kosciug <bartosz@cloudify.co>
2022-11-19 19:20:30 +0100

    Adding junitparser lib.

--- catalog.py
+++ catalog.py
@@ -5,6 +5,7 @@ import os
 import re
 import shutil
 import xml.etree.ElementTree as ET
+from junitparser import JUnitXml
 
 import yaml
 from pygit2 import Repository
@@ -16,8 +17,8 @@ BP_NAME = re.compile("(?<=\[)(.*)(?=\])")
 
 def read_xml(path):
     try:
-        tree = ET.parse(path)
-        return tree.getroot() #return test suites
+        test_suites =JUnitXml.fromfile(path)
+        return test_suites
     except FileNotFoundError:
         logging.info(
             'The test result file was not found under: {} path'.format(path))
@@ -29,9 +30,8 @@ def get_broken_bps_ids():
     if test_suites:
         for suite in test_suites:   
             for test_case in suite:
-                match = BP_NAME.search(test_case.attrib.get('name'))
-                if match: 
-                    broken_bps.append(match[0])
+                if not test_case.is_passed:
+                    broken_bps.append(BP_NAME.search(test_case.name)[0])
     return broken_bps
 
 def create_build_directories():
