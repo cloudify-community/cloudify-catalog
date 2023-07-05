c0d2f9faff063147e4d39a812ad93c6d0cb3a1e2
bartoszkosciug <97445164+bartoszkosciug@users.noreply.github.com>
2023-07-05 10:54:52 +0200

    RND-229 Adding generic security group. (#437)
    * Adding generic security group.
    
    * Change depends on.
    
    * Service component security groups.
    
    * Adding description to deployment_id in main bp.
    
    * Remove main bp field form security group utility.

---
+++ tabs/utilities/security_group/providers/scripts/generate_bp.py
@@ -0,0 +1,85 @@
+import os
+import shutil
+import sys
+import zipfile
+from os.path import dirname
+from string import ascii_letters, ascii_lowercase
+
+import yaml
+from cloudify import ctx
+from cloudify.state import ctx_parameters as inputs
+from cloudify.utils import id_generator
+from jinja2 import Environment, FileSystemLoader
+
+
+def _handle_parent_directory(into_dir):
+    extracted_files = os.listdir(into_dir)
+    ctx.logger.info(extracted_files)
+    if len(extracted_files) == 1:
+        inner_dir = os.path.join(into_dir, extracted_files[0])
+        if os.path.isdir(inner_dir):
+            return inner_dir
+    return into_dir
+
+
+def unzip_archive(archive_path, skip_parent_directory=True):
+    """
+    Unzip a zip archive.
+    this method memic strip components
+    """
+    into_dir = dirname(__file__)
+    ctx.logger.info("Unzipping to {}".format(into_dir))
+    zip_in = None
+    try:
+        with zipfile.ZipFile(archive_path, 'r') as zip_ref:
+            zip_ref.extractall(into_dir)
+            for info in zip_ref.infolist():
+                reset_target = os.path.join(into_dir, info.filename)
+                if info.external_attr >> 16 > 0:
+                    os.chmod(reset_target, info.external_attr >> 16)
+        if skip_parent_directory:
+            into_dir = _handle_parent_directory(into_dir)
+    except zipfile.BadZipFile as e:
+        # clean up that temp directory and raise the exception
+        shutil.rmtree(into_dir)
+        raise e
+    finally:
+        if zip_in:
+            zip_in.close()
+            os.remove(archive_path)
+    return into_dir
+
+
+class_path = ctx.download_resource('scripts/templates.zip')
+unziped_class = unzip_archive(class_path, False)
+
+ports = inputs["ports"]
+provider = inputs["provider"]
+
+if provider == 'aws':
+    rules = [{"from_port": port, "to_port": port} for port in ports]
+elif provider == 'azure':
+    rules = []
+    ids = []
+    while len(ids) < len(ports):
+        new_id = ''.join(id_generator(size=6, chars=ascii_lowercase))
+        if new_id not in ids:
+            ids.append(new_id)
+    rules = [
+        {"port": port, "description": "Access-rule-{0}".format(ids[i]),
+         "name": "Rule-{0}".format(ids[i]),
+         "prority": str(101 + i)} for i, port in enumerate(ports)]
+
+path = os.path.join(unziped_class, 'templates/')
+environment = Environment(loader=FileSystemLoader(path), autoescape=True)
+
+results_filename = "{}.j2".format(provider)
+results_template = environment.get_template(results_filename)
+
+context = {
+    "rules": rules
+}
+
+ctx.instance.runtime_properties['value'] = yaml.safe_load(
+    results_template.render(context))
+ctx.instance.runtime_properties['sg_suffix'] = '-auto-gen-'+id_generator(size=6, chars=ascii_lowercase)
